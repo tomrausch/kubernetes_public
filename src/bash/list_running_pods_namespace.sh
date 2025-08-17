1cdd6f105540804e24aa8729d4b5bd903d3ecbf2
tomrausch <tomrausch@hotmail.com>
2025-08-17 07:58:58 -0500

    Create list_running_pods_namespace.sh

---
+++ src/bash/list_running_pods_namespace.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Reference: https://medium.com/p/24bc24c867e8
+NAMESPACE = $1  # Takes namespace as an argument
+if [ -z "$NAMESPACE" ]; then
+  echo "Usage: $0 <namespace>"
+  exit 1
+fi
+kubectl get pods -n "$NAMESPACE" --field-selector=status.phase=Running
