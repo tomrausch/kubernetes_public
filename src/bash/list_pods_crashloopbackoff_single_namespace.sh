3221f438657d9c1515f3975c7da23a19ae5a0af8
tomrausch <tomrausch@hotmail.com>
2025-08-17 13:37:35 -0500

    Update list_pods_crashloopbackoff_single_namespace.sh

--- src/bash/list_pods_crashloopbackoff_single_namespace.sh
+++ src/bash/list_pods_crashloopbackoff_single_namespace.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Purpose: List All Pods In State "CrashLoopBackoff" In A Single Namespace
+# Reference: https://medium.com/devops-dev/top-16-kubernetes-scripts-for-streamlined-cluster-management-24bc24c867e8
 NAMESPACE=$1
 if [ -z "$NAMESPACE" ]; then
   echo "Usage: $0 <namespace>"
