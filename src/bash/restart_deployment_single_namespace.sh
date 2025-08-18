ef5178f7ab334499ce264ec8c25cddf8a8ee82eb
tomrausch <tomrausch@hotmail.com>
2025-08-18 13:11:38 -0500

    Update restart_deployment_single_namespace.sh

--- src/bash/restart_deployment_single_namespace.sh
+++ src/bash/restart_deployment_single_namespace.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Description: Restart all Deployments in a single namespace
+# Reference: https://medium.com/devops-dev/top-16-kubernetes-scripts-for-streamlined-cluster-management-24bc24c867e8
 NAMESPACE=$1
 if [ -z "$NAMESPACE" ]; then
   echo "Usage: $0 <namespace>"
