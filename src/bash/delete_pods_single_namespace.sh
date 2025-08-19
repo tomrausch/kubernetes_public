c4a0aa300f4bdd68add15e830e6cdbec9a53b41e
tomrausch <tomrausch@hotmail.com>
2025-08-19 09:14:33 -0500

    Create delete_pods_single_namespace.sh

---
+++ src/bash/delete_pods_single_namespace.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Description: Delete all Pods (Restart Pods) in a single namespace
+# Reference:   https://medium.com/devops-dev/top-16-kubernetes-scripts-for-streamlined-cluster-management-24bc24c867e8
+NAMESPACE=$1
+if [ -z "$NAMESPACE" ]; then
+  echo "Usage: $0 <namespace>"
+  exit 1
+fi
+kubectl delete pod --all -n "$NAMESPACE"
