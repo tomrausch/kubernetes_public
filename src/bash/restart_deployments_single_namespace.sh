dca7a250f818794fff468abfa5b0b4f7e4a3e1d4
tomrausch <tomrausch@hotmail.com>
2025-08-19 22:52:26 -0500

    Update restart_deployments_single_namespace.sh

--- src/bash/restart_deployments_single_namespace.sh
+++ src/bash/restart_deployments_single_namespace.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# Description: Restart all Deployments in a single namespace
+# Description: Restart all Deployments in a single Namespace
 # Reference: https://medium.com/devops-dev/top-16-kubernetes-scripts-for-streamlined-cluster-management-24bc24c867e8
-NAMESPACE="${1:-default}"
-if [ -z "$NAMESPACE" ]; then
-  echo "Usage: $0 <namespace>"
+if [ "$1" == "-h" ]; then
+  echo "Usage  : $0 <namespace>"
+  echo "Default: '<namespace>' = 'default'"
+  echo "Command: kubectl rollout restart deployment -n <namespace>"
   exit 1
 fi
+NAMESPACE="${1:-default}"
 kubectl rollout restart deployment -n "$NAMESPACE"
