6c339b695b334799fc6a7810796f372b8b6f85b7
tomrausch <tomrausch@hotmail.com>
2025-08-20 10:37:43 -0500

    Update top_pods_single_namespace.sh

--- src/bash/top_pods_single_namespace.sh
+++ src/bash/top_pods_single_namespace.sh
@@ -4,7 +4,7 @@
 if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
   echo "Usage  : $0 <namespace>"
   echo "Default: '<namespace>' = 'default'"
-  echo "Command: kubectl top pods -n <namespace>'"
+  echo "Command: kubectl top pods -n <namespace>"
   exit 1
 fi
 NAMESPACE="${1:-default}"
