ebbb9891d0389c1014521b787d74327a3c5718af
tomrausch <tomrausch@hotmail.com>
2025-08-19 22:45:49 -0500

    Update get_pods_crashloopbackoff_single_namespace.sh

--- src/bash/get_pods_crashloopbackoff_single_namespace.sh
+++ src/bash/get_pods_crashloopbackoff_single_namespace.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# Description: Get (list) all Pods in Status.Phase = "Running" and Reason = "CrashLoopBackoff" in a single namespace
+# Description: Get (list) all Pods in Status.Phase = "Running" and Reason = "CrashLoopBackoff" in a single Namespace
 # Reference:   https://medium.com/devops-dev/top-16-kubernetes-scripts-for-streamlined-cluster-management-24bc24c867e8
-NAMESPACE="${1:-default}"
-if [ -z "$NAMESPACE" ]; then
-  echo "Usage: $0 <namespace>"
+if [ "$1" == "-h" ]; then
+  echo "Usage  : $0 <namespace>"
+  echo "Default: '<namespace>' = 'default'"
+  echo "Command: kubectl get pods -n <namespace> --field-selector=status.phase=Running -o json | jq -r '.items[] | select(.status.containerStatuses[]?.state.waiting?.reason=="CrashLoopBackOff") | .metadata.name'
   exit 1
 fi
+NAMESPACE="${1:-default}"
 kubectl get pods -n "$NAMESPACE" --field-selector=status.phase=Running -o json | jq -r '.items[] | select(.status.containerStatuses[]?.state.waiting?.reason=="CrashLoopBackOff") | .metadata.name'
