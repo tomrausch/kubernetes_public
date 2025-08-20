f782e40ce19ae350d9b15d03cdb1a9edcd9eae39
tomrausch <tomrausch@hotmail.com>
2025-08-19 21:57:39 -0500

    Update get_pods_crashloopbackoff_single_namespace.sh

--- src/bash/get_pods_crashloopbackoff_single_namespace.sh
+++ src/bash/get_pods_crashloopbackoff_single_namespace.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Description: Get (list) all Pods in Status.Phase = "Running" and Reason = "CrashLoopBackoff" in a single namespace
 # Reference:   https://medium.com/devops-dev/top-16-kubernetes-scripts-for-streamlined-cluster-management-24bc24c867e8
-NAMESPACE=$1
+NAMESPACE="${1:-default}"
 if [ -z "$NAMESPACE" ]; then
   echo "Usage: $0 <namespace>"
   exit 1
