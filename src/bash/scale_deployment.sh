0a95117b2883839d8d4702f413b5287f4af666ae
tomrausch <tomrausch@hotmail.com>
2025-08-20 09:37:50 -0500

    Update scale_deployment.sh

--- src/bash/scale_deployment.sh
+++ src/bash/scale_deployment.sh
@@ -3,7 +3,7 @@
 # Reference:   https://medium.com/devops-dev/top-16-kubernetes-scripts-for-streamlined-cluster-management-24bc24c867e8
 if [ "$1" == "-h" ] || [ "$1" == "--help" ] ; then
   echo "Usage  : $0 <deployment> <replicas> <namespace>"
-  echo "Defaults:""
+  echo "Defaults:"
   echo "- '<deployment>' = 'it-tools'"
   echo "- '<replicas>' = '1'"
   echo "- '<namespace>' = 'default'"
