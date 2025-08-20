3c8f626665dd709dbcec486b6508b15229d59213
tomrausch <tomrausch@hotmail.com>
2025-08-19 22:14:53 -0500

    Update create_token_kubernetes-dashboard.sh

--- src/bash/create_token_kubernetes-dashboard.sh
+++ src/bash/create_token_kubernetes-dashboard.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # Description: Create a token for the Kubernetes Dashboard
 # Reference:   Front page of Kubernetes Dashboard
+if [[ "$1" == "-h" ]]; then
+  echo "Usage: $0"
+  echo "Command: kubectl -n kubernetes-dashboard create token dashboard-admin-sa"
+  exit 1
+fi
 kubectl -n kubernetes-dashboard create token dashboard-admin-sa
