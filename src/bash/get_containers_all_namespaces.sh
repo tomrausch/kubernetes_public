1912358674bda70a7443f07ae112888a00c44b87
tomrausch <tomrausch@hotmail.com>
2025-08-20 14:50:18 -0500

    Update get_containers_all_namespaces.sh

--- src/bash/get_containers_all_namespaces.sh
+++ src/bash/get_containers_all_namespaces.sh
@@ -3,7 +3,7 @@
 # Reference:   https://medium.com/devops-dev/top-16-kubernetes-scripts-for-streamlined-cluster-management-24bc24c867e8
 if [ "$1" == "-h" ]; then
   echo "Usage  : $0"
-  echo "Command: kubectl get pods --all-namespaces -o=custom-columns=NAMESPACE:.metadata.namespace,POD:.metadata.name,CONTAINER\(S\):.spec.containers[*].name,INIT-CONTAINER\(S\):.spec.initContainers[*].name
+  echo "Command: kubectl get pods --all-namespaces -o=custom-columns=NAMESPACE:.metadata.namespace,POD:.metadata.name,CONTAINER\(S\):.spec.containers[*].name,INIT-CONTAINER\(S\):.spec.initContainers[*].name"
   exit 1
 fi
 kubectl get pods --all-namespaces -o=custom-columns=NAMESPACE:.metadata.namespace,POD:.metadata.name,CONTAINER\(S\):.spec.containers[*].name,INIT-CONTAINER\(S\):.spec.initContainers[*].name
