11206f377f293b0826917e757476a87604141e7e
tomrausch <tomrausch@hotmail.com>
2025-08-19 22:18:08 -0500

    Update display_logs_match_label_single_namespace.sh

--- src/bash/display_logs_match_label_single_namespace.sh
+++ src/bash/display_logs_match_label_single_namespace.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 # Description: Display Logs of all Pods matching a specific Label in a single Namespace
 # Reference:   https://medium.com/devops-dev/top-16-kubernetes-scripts-for-streamlined-cluster-management-24bc24c867e8
-LABEL="$1"
-NAMESPACE="${2:-default}"
-if [ -z "$LABEL" ]; then
+if [[ "$1" == "-h" ]]; then
   echo "Usage: $0 <label> <namespace>"
-  echo "Example: '<label>' = 'app=nginx'"
   echo "Default: '<namespace>' = 'default'"
+  echo "Command: kubectl get pods -n "$NAMESPACE" -l "$LABEL" -o name"
   exit 1
 fi
-echo "LABEL     = $LABEL"
-echo "NAMESPACE = $NAMESPACE"
+LABEL="$1"
+NAMESPACE="${2:-default}"
 for POD in $(kubectl get pods -n "$NAMESPACE" -l "$LABEL" -o name); do
   echo "=== $POD ==="
   kubectl logs -f -n "$NAMESPACE" "$POD" &
