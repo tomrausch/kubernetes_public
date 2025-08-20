d9b4fe2b0146f3143cefdda0be6e45051df2efad
tomrausch <tomrausch@hotmail.com>
2025-08-19 22:14:20 -0500

    Update delete_jobs_successful_single_namespace.sh

--- src/bash/delete_jobs_successful_single_namespace.sh
+++ src/bash/delete_jobs_successful_single_namespace.sh
@@ -3,6 +3,7 @@
 # Reference:   https://medium.com/devops-dev/top-16-kubernetes-scripts-for-streamlined-cluster-management-24bc24c867e8
 if [[ "$1" == "-h" ]]; then
   echo "Usage: $0 <namespace>"
+  echo "Command: kubectl delete jobs -n <namespace> --field-selector=status.successful=1"
   exit 1
 fi
 NAMESPACE="${1:-default}"
