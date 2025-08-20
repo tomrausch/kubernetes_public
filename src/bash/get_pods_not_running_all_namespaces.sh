b68f1c40a5e584c5f031326ef65180b1bb88f624
tomrausch <tomrausch@hotmail.com>
2025-08-19 22:47:28 -0500

    Update get_pods_not_running_all_namespaces.sh

--- src/bash/get_pods_not_running_all_namespaces.sh
+++ src/bash/get_pods_not_running_all_namespaces.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Description: Get (list) all Pods not in Status.Phase = 'Running' and not in Status.Phase = 'Succeeded' in all namespaces
+# Description: Get (list) all Pods not in Status.Phase = 'Running' and not in Status.Phase = 'Succeeded' in all Namespaces
 # Reference:   https://medium.com/@obaff/10-bash-scripts-for-troubleshooting-kubernetes-issues-cde367ae4ac3
 if [ "$1" == "-h" ]; then
   echo "Usage  : $0"
