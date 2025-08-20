685a35724e8fb54bf41332cae37a3ba46b58ce37
tomrausch <tomrausch@hotmail.com>
2025-08-20 14:08:50 -0500

    Update top_pods_all_namespaces.sh

--- src/bash/top_pods_all_namespaces.sh
+++ src/bash/top_pods_all_namespaces.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # Description: Top (get CPU and Memory utilization) for all Pods in all Namespaces
+#              (Optional) Sort by "cpu" or by "memory"
 # Reference:   https://medium.com/devops-dev/top-16-kubernetes-scripts-for-streamlined-cluster-management-24bc24c867e8
 if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-  echo "Usage  : $0"
-  echo "Command: kubectl top pods --all-namespaces"
+  echo "Usage  : $0 [<sortby>: cpu|memory]"
+  echo "Command: kubectl top pods --all-namespaces [--sort-by=<sortby>]"
   exit 1
 fi
-kubectl top pods --all-namespaces
+SORTBY="$1"
+if [ -z "$SORTBY"]; then
+  kubectl top pods --all-namespaces
+else
+ kubectl top pods --all-namespaces --sort-by="$SORTBY"
+fi
