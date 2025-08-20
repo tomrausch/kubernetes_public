fbe95fc32a424fa83165d6864789076a6abb9dfe
tomrausch <tomrausch@hotmail.com>
2025-08-19 22:47:08 -0500

    Update get_pods_not_running_all_namespaces.sh

--- src/bash/get_pods_not_running_all_namespaces.sh
+++ src/bash/get_pods_not_running_all_namespaces.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # Description: Get (list) all Pods not in Status.Phase = 'Running' and not in Status.Phase = 'Succeeded' in all namespaces
 # Reference:   https://medium.com/@obaff/10-bash-scripts-for-troubleshooting-kubernetes-issues-cde367ae4ac3
+if [ "$1" == "-h" ]; then
+  echo "Usage  : $0"
+  echo "kubectl get pods --all-namespaces --field-selector=status.phase!=Running,status.phase!=Succeeded -o wide"
+  exit 1
+fi
 kubectl get pods --all-namespaces --field-selector=status.phase!=Running,status.phase!=Succeeded -o wide
