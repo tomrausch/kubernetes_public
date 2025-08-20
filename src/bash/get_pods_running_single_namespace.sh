1a038bbd52983dd0a58532557088f0d94f2ba47e
tomrausch <tomrausch@hotmail.com>
2025-08-19 22:49:32 -0500

    Update get_pods_running_single_namespace.sh

--- src/bash/get_pods_running_single_namespace.sh
+++ src/bash/get_pods_running_single_namespace.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# Description: Get (list) all Pods in Status.Phase = 'Running' in a single namespace
+# Description: Get (list) all Pods in Status.Phase = 'Running' in a single Namespace
 # Reference: https://medium.com/p/24bc24c867e8
-NAMESPACE="${1:-default}"
-if [ -z "$NAMESPACE" ]; then
-  echo "Usage: $0 <namespace>"
+if [ "$1" == "-h" ]; then
+  echo "Usage  : $0 <namespace>"
+  echo "Default: '<namespace>' = 'default'"
+  echo "Command: kubectl get pods -n <namespace> --field-selector=status.phase=Running"
   exit 1
 fi
+NAMESPACE="${1:-default}"
 kubectl get pods -n "$NAMESPACE" --field-selector=status.phase=Running
